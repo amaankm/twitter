d704b5b85b7f5153760f6d874140f1b3e6c59551
Amaan <amaankareem4@gmail.com>
2024-06-28 20:20:14 +0530

    auth controllers

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import {
+  signup,
+  login,
+  logout,
+  getMe,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
@@ -8,5 +14,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 
 router.post("/logout", logout);
+
+router.get("/me", protectRoute, getMe);
 
 export default router;
